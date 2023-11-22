1bb90362c292ede808a02363e30be24729611047
Fernandez-Sebastian <fernandez90dgo@gmail.com>
2023-11-22 00:51:38 -0300

    ajuste

--- src/Components/ContadorResponsive/ContadorResponsive.jsx
+++ src/Components/ContadorResponsive/ContadorResponsive.jsx
@@ -3,8 +3,10 @@ import {
         Text,
         Button,
         ChakraProvider,
-        Box 
+        Box, 
+        Flex
     } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 
 function ContadorResponsive() {
@@ -30,30 +32,35 @@ function ContadorResponsive() {
         <ChakraProvider>
             <Box pt="30px"> 
                 <Text as='h1' id='contar' textAlign="center" fontFamily="cursive"> Contador: {contador}</Text>
-                <Box textAlign="center">
-                    <Button 
-                    bg='#438e96' color="white"_hover={{bg:"#356169"}}
-                    onClick={decrementar}isDisabled={
-                        contador==(-15)?"isDisabled":""
-                    }
-                    m="5px"
-                    >Decrementar
-                    </Button>
-                    <Button 
-                    bg='#438e96'color="white" _hover={{bg:"#356169"}}
-                    onClick={resetear}isDisabled={
-                        contador==(0)?"isDisabled":""
-                    }
-                    m="5px"
-                    >Resetear
-                    </Button>
-                    <Button 
-                    bg='#438e96' color="white" _hover={{bg:"#356169"}}
-                    onClick={incrementar} isDisabled={contador==15?"isDisabled":""} 
-                    m="5px"
-                    >Incrementar
-                    </Button>
-                </Box>   
+                <Flex w="100%" justifyContent="center" >
+                    <Flex w={{base:"90%", sm:"50%", md:"25%" }} mt="50px" justifyContent="space-between">
+                        <motion.div whileHover={contador==-15 || contador==15  ? { scale: 1 } : { scale: 1.2 }} whileTap={contador==-15 || contador==15  ? { scale: 1 } : { scale: 0.8 }} w="fit-content" h="fit-content">
+                            <Button 
+                            bg='#438e96' color="white"_hover={{bg:"#356169"}}
+                            onClick={decrementar}isDisabled={
+                                contador==(-15)?"isDisabled":""
+                            }
+                            >Decrementar
+                            </Button>
+                        </motion.div>
+                        <motion.div whileHover={contador==0 ? { scale: 1 } : { scale: 1.2 }} whileTap={contador==0 ? { scale: 1 } : { scale: 0.8 }} w="fit-content" h="fit-content">
+                            <Button 
+                            bg='#438e96'color="white" _hover={{bg:"#356169"}}
+                            onClick={resetear}isDisabled={
+                                contador==(0)?"isDisabled":""
+                            }
+                            >Resetear
+                            </Button>
+                        </motion.div>
+                        <motion.div whileHover={contador==-15 || contador==15  ? { scale: 1 } : { scale: 1.2 }} whileTap={contador==-15 || contador==15  ? { scale: 1 } : { scale: 0.8 }} w="fit-content" h="fit-content">
+                            <Button 
+                            bg='#438e96' color="white" _hover={{bg:"#356169"}}
+                            onClick={incrementar} isDisabled={contador==15?"isDisabled":""} 
+                            >Incrementar
+                            </Button>
+                        </motion.div>
+                    </Flex>  
+                </Flex> 
             </Box>
         </ChakraProvider>
     </>
